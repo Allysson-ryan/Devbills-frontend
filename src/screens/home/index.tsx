876006dedf796f7cc47c07b6dc948184750b82b1
Allysson Ryan <allyssonr002@gmail.com>
2025-01-04 21:02:15 -0300

    adicionando código-fonte

---
+++ src/screens/home/index.tsx
@@ -0,0 +1,45 @@
+import { Filters, Header, InputGroup, Main, Section } from './style';
+import { Logo } from './../../components/logo/index';
+import { Button } from '../../components/button';
+import { Title } from '../../components/title';
+import { Input } from '../../components/input';
+import { InputMask } from '@react-input/mask';
+
+export function Home() {
+  return (
+    <>
+      <Header>
+        <Logo />
+        <div>
+          <Button>Nova transação</Button>
+          <Button>Nova categoria</Button>
+        </div>
+      </Header>
+      <Main>
+        <Section>
+          <Filters>
+            <Title title="Saldo" subtitle="Receitas e despesas no período" />
+            <InputGroup>
+              <InputMask
+                component={Input}
+                mask="dd/mm/yyyy"
+                replacement={{ d: /\d/, m: /\d/, y: /\d/ }}
+                variant="dark"
+                label="Início"
+                placeholder="dd/mm/yyyy"
+              />
+              <InputMask
+                component={Input}
+                mask="dd/mm/yyyy"
+                replacement={{ d: /\d/, m: /\d/, y: /\d/ }}
+                variant="dark"
+                label="Fim"
+                placeholder="dd/mm/yyyy"
+              />
+            </InputGroup>
+          </Filters>
+        </Section>
+      </Main>
+    </>
+  );
+}
